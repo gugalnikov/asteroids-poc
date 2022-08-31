2969e0f27b22428bb6b00cacb9cd069caa6f3ad5
gugalnikov <gugalnikov@gmail.com>
2022-09-01 00:28:11 +0200

    Initial commit

---
+++ src/main/java/com/example/asteroidspoc/AsteroidsPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.asteroidspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidsPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsteroidsPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asteroidspoc/AsteroidsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asteroidspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidsPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
